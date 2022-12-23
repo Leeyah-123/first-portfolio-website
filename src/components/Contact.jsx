a32f2ea16079c425cc5abaa0fe0d6f6b1e8ab347
Leeyah-123 <junaidaaliyah260@gmail.com>
2022-12-23 02:04:29 +0100

    Setting up sending of email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,49 @@
-import React from 'react';
+import emailjs from '@emailjs/browser';
+import React, { useRef } from 'react';
 
 import '../styles/contact.css';
 
 function Contact() {
+  const nameRef = useRef('');
+  const emailRef = useRef('');
+  const messageRef = useRef('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (
+      nameRef.current.value !== '' &&
+      emailRef.current.value !== '' &&
+      messageRef.current.value !== ''
+    ) {
+      var templateParams = {
+        from_name: nameRef.current.value,
+        from_email: emailRef.current.value,
+        message: messageRef.current.value,
+      };
+      emailjs
+        .send(
+          'service_4idt2jp',
+          'template_rghgj02',
+          templateParams,
+          'zhavXMMluns0vxq_B'
+        )
+        .then(
+          (result) => {
+            nameRef.current.value = '';
+            emailRef.current.value = '';
+            messageRef.current.value = '';
+            // show the user a success message
+            console.log(result);
+          },
+          (error) => {
+            // show the user an error
+            console.log(error);
+          }
+        );
+    }
+  };
+
   return (
     <section id="contact">
       <div className="contact">
@@ -46,11 +87,24 @@ function Contact() {
           </a>
         </div>
         <div className="contact-form-container">
-          <form className="contact-form">
-            <input type="text" placeholder="Your Name" required />
-            <input type="email" placeholder="Your Email" required />
-            <textarea placeholder="Message" cols="30" rows="10" required />
-            <button type="submit">submit</button>
+          <form className="contact-form" onSubmit={handleSubmit}>
+            <input type="text" ref={nameRef} placeholder="Your Name" required />
+            <input
+              type="email"
+              ref={emailRef}
+              placeholder="Your Email"
+              required
+            />
+            <textarea
+              ref={messageRef}
+              placeholder="Message"
+              cols="30"
+              rows="10"
+              required
+            />
+            <button type="submit" onSubmit={handleSubmit}>
+              submit
+            </button>
           </form>
         </div>
       </div>
